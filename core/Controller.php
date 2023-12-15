fbf89e437525a799ad25d08b88207dec90147391
Namikazes <egorsitenko35@gmail.com>
2023-12-15 17:41:35 +0200

    [hw-11]MVC-Router-Controller

--- core/Controller.php
+++ core/Controller.php
@@ -1,8 +1,12 @@
 <?php
 
-namespace core;
+namespace Core;
 
-class Controller
+abstract class Controller
 {
-
+    public function before(string $action, array $params = []): bool
+    {
+        return true;
+    }
+    public function after(string $action) {}
 }
--- public/index.php
+++ public/index.php
@@ -2,13 +2,13 @@
 
 define('BASE_DIR', dirname(__DIR__));
 
-require_once BASE_DIR . "/Config/constants.php";
+require_once BASE_DIR . "/config/constants.php";
 require_once BASE_DIR . "/vendor/autoload.php";
-
+require_once BASE_DIR . "/core/Router.php";
 
 try{
     if(!preg_match('/assets/i', $_SERVER['REQUEST_URI'])) {
-        \core\Router::dispatch($_SERVER['REQUEST_URI']);
+        \Core\Router::dispatch($_SERVER['REQUEST_URI']);
     }
 } catch (PDOException $exception){
     dd("PDOException", $exception);
